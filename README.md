8142d88f36971d0d9b710dacba73a2fa23ce4fbd
Jacob Roush <jacobroushmail@gmail.com>
2019-10-30 01:20:18 -0500

    working

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Finding Ferrell Python Demo
 
 Learn just how powerful and fun it is to write in Python by using the `face_recognition` library to detect Will Ferrell in photos.
+
+Two main libraries used where:
+[Facial Recognition Library](https://github.com/ageitgey/face_recognition)
+
+[Image Manipulation using Pillow](https://github.com/python-pillow/Pillow)
+
+## How to Run
+
+Verify Python is installed on your machine by running `python -V`.
+
+Make sure Pillow and facial_recognition are installed via `pip`.
--- face-detect.py
+++ face-detect.py
@@ -1,1 +1,38 @@
+# Manipulate images with the Pillow Library
+from PIL import Image, ImageFont, ImageDraw
+# import face detection from when we installed with pip
 import face_recognition
+
+ferrellReferencePhoto = face_recognition.load_image_file("./Will-Ferrell.jpg")
+
+# This variable stores all the facial features encoded in an array which face_recognition will use to compare
+ferrellReferencePhotoEncoded = face_recognition.face_encodings(ferrellReferencePhoto)[0]
+
+numberPicker = input("Pick a number 1-4: ")
+
+newPhoto = face_recognition.load_image_file("./examples/ferrell-" + numberPicker + ".jpg")
+
+# locations are saved as top, bottom, left, right. Get encoding of new photo
+newPhotoFaceLocations = face_recognition.face_locations(newPhoto)
+newPhotoEncoded = face_recognition.face_encodings(newPhoto, newPhotoFaceLocations)
+
+newPhotoModified = Image.open("./examples/ferrell-" + numberPicker + ".jpg")
+
+# All of our drawing
+canvas = ImageDraw.Draw(newPhotoModified)
+
+# Gives an array of which faces are Will's
+results = face_recognition.compare_faces(
+    ferrellReferencePhotoEncoded, newPhotoEncoded)
+
+font = ImageFont.truetype("Roboto-Black.ttf", 38, encoding="unic")
+
+for index, faces in enumerate(newPhotoFaceLocations):
+    color = "black"
+    if (results[index]):
+        color = "red"
+        canvas.text((faces[3], faces[2]), "Ferrell Located", "White", font)
+
+    canvas.rectangle(((faces[3], faces[0]), (faces[1], faces[2])), outline=color)
+
+newPhotoModified.save("output.jpeg", "JPEG")
